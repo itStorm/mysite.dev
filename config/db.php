dd7ea35013419affd1fc41579e2e4bd80736b529
v.krasikov <vl.itstorm@gmail.com>
2015-05-28 01:15:32 +0300

    DB settings & create user table

--- config/db.php
+++ config/db.php
@@ -2,8 +2,8 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
+    'dsn' => 'mysql:host=localhost;dbname=mysite',
     'username' => 'root',
-    'password' => '',
+    'password' => '12344321',
     'charset' => 'utf8',
 ];
