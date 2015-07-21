ab9cae1e1dcf460a7aa9b114c240a9d497fca6fb
v.krasikov <vl.itstorm@gmail.com>
2015-07-22 00:31:23 +0300

    Change main page. Add carousel and greed add blocks on layout

--- modules/main/views/default/index.php
+++ modules/main/views/default/index.php
@@ -1,51 +1,52 @@
 <?php
+use yii\bootstrap\Carousel;
+
 /* @var $this yii\web\View */
 $this->title = 'My Yii Application';
 ?>
+
+
+<?php
+$this->beginBlock('before_content');
+echo Carousel::widget ( [
+    'items' => [
+        [
+            'content' => '<div class="img"><img src="//:0"/></div>',
+            'caption' => '<h2>Article caption 1</h2><p>announcement text 1 .......</p>',
+            'options' => []
+        ],
+        [
+            'content' => '<div class="img"><img src="//:0"/></div>',
+            'caption' => '<h2>Article caption 2</h2><p>announcement text 2 .......</p>',
+            'options' => []
+        ],
+    ],
+]);
+$this->endBlock();
+?>
+
+<?php $this->beginBlock('sidebar') ?>
+    MENU
+<?php $this->endBlock() ?>
+
 <div class="site-index">
 
-    <div class="jumbotron">
-        <h1>Congratulations!</h1>
-
-        <p class="lead">You have successfully created your Yii-powered application.</p>
-
-        <p><a class="btn btn-lg btn-success" href="http://www.yiiframework.com">Get started with Yii</a></p>
-    </div>
-
-    <div class="body-content">
-
-        <div class="row">
-            <div class="col-lg-4">
-                <h2>Heading</h2>
-
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
-
-                <p><a class="btn btn-default" href="http://www.yiiframework.com/doc/">Yii Documentation &raquo;</a></p>
-            </div>
-            <div class="col-lg-4">
-                <h2>Heading</h2>
-
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
-
-                <p><a class="btn btn-default" href="http://www.yiiframework.com/forum/">Yii Forum &raquo;</a></p>
-            </div>
-            <div class="col-lg-4">
-                <h2>Heading</h2>
-
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
-
-                <p><a class="btn btn-default" href="http://www.yiiframework.com/extensions/">Yii Extensions &raquo;</a></p>
-            </div>
+    <div class="row">
+        <div class="col-sm-4">
+            <h2>Header 1</h2>
+            <p>Text 1</p>
+            <p><a class="btn btn-default" href="#">more... &raquo;</a></p>
         </div>
-
+        <div class="col-sm-4">
+            <h2>Header 2</h2>
+            <p>Text 2</p>
+            <p><a class="btn btn-default" href="#">more... &raquo;</a></p>
+        </div>
+        <div class="col-sm-4">
+            <h2>Header 3</h2>
+            <p>Text 3</p>
+            <p><a class="btn btn-default" href="#">more... &raquo;</a></p>
+        </div>
     </div>
+
 </div>
--- views/layouts/main.php
+++ views/layouts/main.php
@@ -24,42 +24,56 @@ AppAsset::register($this);
 
 <?php $this->beginBody() ?>
     <div class="wrap">
-        <?php
-            NavBar::begin([
-                'brandLabel' => Yii::$app->params['projectName'],
-                'brandUrl' => Yii::$app->homeUrl,
-                'options' => [
-                    'class' => 'navbar-inverse navbar-fixed-top',
-                ],
-            ]);
-            echo Nav::widget([
-                'options' => ['class' => 'navbar-nav navbar-right'],
-                'items' => [
-                    ['label' => Yii::t('app', 'Home'), 'url' => ['/main/default/index']],
-                    ['label' => Yii::t('app', 'About'), 'url' => ['/main/default/about']],
-                    ['label' => Yii::t('app', 'Contact'), 'url' => ['/main/default/contact']],
-                    Yii::$app->user->isGuest ?
-                        ['label' => Yii::t('app', 'Login'), 'url' => ['/user/default/login']] :
-                        ['label' => Yii::t('app', 'Logout ({username})', ['username' => Yii::$app->user->identity->username ]),
-                            'url' => ['/user/default/logout'],
-                            'linkOptions' => ['data-method' => 'post']],
-                ],
-            ]);
-            NavBar::end();
-        ?>
+        <div class="navbar-wrapper">
+            <div class="container">
+            <?php
+                NavBar::begin([
+                    'brandLabel' => Yii::$app->params['projectName'],
+                    'brandUrl' => Yii::$app->homeUrl,
+                    'options' => [
+                        'class' => 'navbar-inverse  navbar-static-top',
+                    ],
+                ]);
+                echo Nav::widget([
+                    'options' => ['class' => 'navbar-nav navbar-right'],
+                    'items' => [
+                        ['label' => Yii::t('app', 'Home'), 'url' => ['/main/default/index']],
+                        ['label' => Yii::t('app', 'About'), 'url' => ['/main/default/about']],
+                        ['label' => Yii::t('app', 'Contact'), 'url' => ['/main/default/contact']],
+                        Yii::$app->user->isGuest ?
+                            ['label' => Yii::t('app', 'Login'), 'url' => ['/user/default/login']] :
+                            ['label' => Yii::t('app', 'Logout ({username})', ['username' => Yii::$app->user->identity->username ]),
+                                'url' => ['/user/default/logout'],
+                                'linkOptions' => ['data-method' => 'post']],
+                    ],
+                ]);
+                NavBar::end();
+            ?>
+            </div>
+         </div>
 
         <div class="container">
-            <?= Breadcrumbs::widget([
-                'links' => isset($this->params['breadcrumbs']) ? $this->params['breadcrumbs'] : [],
-            ]) ?>
-            <?= $content ?>
+            <?= $this->blocks['before_content']; ?>
+        </div>
+
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-9">
+                    <?= Breadcrumbs::widget([
+                        'links' => isset($this->params['breadcrumbs']) ? $this->params['breadcrumbs'] : [],
+                    ]) ?>
+                    <?= $content ?>
+                </div>
+                <div class="col-sm-2 col-sm-offset-1">
+                    <?= $this->blocks['sidebar'] ;?>
+                </div>
+            </div>
         </div>
     </div>
 
     <footer class="footer">
         <div class="container">
-            <p class="pull-left">&copy; My Company <?= date('Y') ?></p>
-            <p class="pull-right"><?= Yii::powered() ?></p>
+            <p class="pull-right">&copy; <?= Yii::$app->params['projectName'] ?> <?= date('Y') ?></p>
         </div>
     </footer>
 
