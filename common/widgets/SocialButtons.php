e341eca6e44d02779b99376db83d53c37c1c21f7
v.krasikov <vl.itstorm@gmail.com>
2015-10-18 00:24:28 +0300

    Improve social buttons widget

--- common/widgets/SocialButtons.php
+++ common/widgets/SocialButtons.php
@@ -2,14 +2,44 @@
 namespace common\widgets;
 
 use yii\base\Widget;
+use yii\web\View;
 
 class SocialButtons extends Widget
 {
+    public $url = null;
+    public $type = 'website';
+    public $title = null;
+    public $description = null;
+    public $image = null;
+
+    /** @var View */
+    protected $view = null;
+
+    /** @inheritdoc */
     public function run()
+    {
+        $this->registerJs()
+            ->registerMetaTags()
+            ->registerCss();
+
+        $html = <<<HTML
+    <div class="social-buttons">
+        <div class="social-buttons-item" id="vk_like"></div>
+        <div class="fb-like" data-layout="button_count" data-action="like" data-show-faces="false" data-share="false"></div>
+    </div>
+HTML;
+
+        return $html;
+    }
+
+    /**
+     * @return $this
+     */
+    protected function registerJs()
     {
         $js = <<<JS
     VK.init({apiId: 5103140, onlyWidgets: true});
-    VK.Widgets.Like("vk_like", {type: "mini", height: 20});
+    VK.Widgets.Like("vk_like", {type: "button", height: 20});
 
     (function(d, s, id) {
         var js, fjs = d.getElementsByTagName(s)[0];
@@ -19,24 +49,56 @@ class SocialButtons extends Widget
         fjs.parentNode.insertBefore(js, fjs);
     }(document, 'script', 'facebook-jssdk'));
 JS;
-        $view = $this->getView();
-        $view->registerJsFile('//vk.com/js/api/openapi.js?117');
-        $view->registerJs($js);
+        $this->getView()->registerJsFile('//vk.com/js/api/openapi.js?117');
+        $this->getView()->registerJs($js);
 
+        return $this;
+    }
+
+    /**
+     * @return $this
+     */
+    protected function registerMetaTags()
+    {
+        $tagsData = array_combine(
+            [
+                'url',
+                'type',
+                'title',
+                'description',
+                'image',
+            ],
+            [
+                $this->url,
+                $this->type,
+                $this->title,
+                $this->description,
+                $this->image,
+            ]);
+        $tagsData = array_filter($tagsData);
+
+        foreach ($tagsData as $property => $value) {
+            $this->getView()->registerMetaTag([
+                'property' => 'og:' . $property,
+                'content'  => $value
+            ]);
+        }
+
+        return $this;
+    }
+
+    /**
+     * @return $this
+     */
+    protected function registerCss()
+    {
         $css = <<<CSS
 .social-buttons-item {
     float: left;
 }
 CSS;
-        $view->registerCss($css);
+        $this->getView()->registerCss($css);
 
-        $html = <<<HTML
-    <div class="social-buttons">
-        <div class="social-buttons-item" id="vk_like"></div>
-        <div class="social-buttons-item fb-like"  data-layout="button_count" data-action="like" data-show-faces="true" data-share="true"></div>
-    </div>
-HTML;
-
-        return $html;
+        return $this;
     }
 }
--- modules/test/views/default/index.php
+++ modules/test/views/default/index.php
@@ -3,3 +3,10 @@
 <?php echo time();?>
 <br>
 <?php echo date('Y-m-d H:i:s T');?>
+
+<br><br>
+<?php
+    echo common\widgets\SocialButtons::widget([
+        'url' => '/test/ololo12',
+    ]);
+?>
