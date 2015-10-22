ce3fc0b25c7c9bc60a54dd9a3cf47c1311644445
v.krasikov <vl.itstorm@gmail.com>
2015-10-23 00:33:59 +0300

    Change database structure

--- migrations/m150723_205147_create_articles_table.php
+++ migrations/m150723_205147_create_articles_table.php
@@ -8,14 +8,16 @@ class m150723_205147_create_articles_table extends Migration
     public function up()
     {
         $this->createTable('articles', [
-            'id'      => Schema::TYPE_PK,
-            'title'   => Schema::TYPE_STRING . '(255) DEFAULT ""',
-            'content' => Schema::TYPE_TEXT,
-            'created' => Schema::TYPE_TIMESTAMP,
-            'updated' => Schema::TYPE_TIMESTAMP,
-            'user_id' => Schema::TYPE_INTEGER,
+            'id'         => Schema::TYPE_PK,
+            'title'      => Schema::TYPE_STRING . '(255) DEFAULT ""',
+            'content'    => Schema::TYPE_TEXT,
+            'created'    => Schema::TYPE_TIMESTAMP,
+            'updated'    => Schema::TYPE_TIMESTAMP,
+            'created_by' => Schema::TYPE_INTEGER . ' NOT NULL',
+            'updated_by' => Schema::TYPE_INTEGER . ' NOT NULL',
         ]);
-        $this->addForeignKey('user_id', 'articles', 'user_id', 'users', 'id', 'RESTRICT', 'CASCADE');
+        $this->addForeignKey('articles_created_by', 'articles', 'created_by', 'users', 'id', 'RESTRICT', 'CASCADE');
+        $this->addForeignKey('articles_updated_by', 'articles', 'updated_by', 'users', 'id', 'RESTRICT', 'CASCADE');
     }
 
     public function down()
--- migrations/m151020_205527_create_tags_table.php
+++ migrations/m151020_205527_create_tags_table.php
@@ -15,9 +15,13 @@ class m151020_205527_create_tags_table extends Migration
 
 
         $this->createTable('article_tag', [
-            'article_id' => Schema::TYPE_INTEGER,
-            'tag_id'     => Schema::TYPE_INTEGER,
+            'article_id' => Schema::TYPE_INTEGER . ' NOT NULL',
+            'tag_id'     => Schema::TYPE_INTEGER . ' NOT NULL',
         ]);
+
+        $this->addForeignKey('article_tag_article_id', 'article_tag', 'article_id', 'articles', 'id', 'RESTRICT', 'CASCADE');
+        $this->addForeignKey('article_tag_tag_id', 'article_tag', 'tag_id', 'tags', 'id', 'RESTRICT', 'CASCADE');
+        $this->createIndex('article_tag_article_id_tag_id', 'article_tag', ['article_id', 'tag_id'], true);
     }
 
     public function down()
