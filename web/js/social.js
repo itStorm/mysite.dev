85942a3471eb2d6db30c601cf512a2f37faf9258
v.krasikov <vl.itstorm@gmail.com>
2015-11-26 12:54:56 +0300

    Change facebook connection code

--- web/js/social.js
+++ web/js/social.js
@@ -2,10 +2,18 @@
 VK.init({apiId: 5103140, onlyWidgets: true});
 
 // Facebook js sdk
-(function(d, s, id) {
+window.fbAsyncInit = function() {
+    FB.init({
+        appId      : '778342298941480',
+        xfbml      : true,
+        version    : 'v2.5'
+    });
+};
+
+(function(d, s, id){
     var js, fjs = d.getElementsByTagName(s)[0];
-    if (d.getElementById(id)) return;
+    if (d.getElementById(id)) {return;}
     js = d.createElement(s); js.id = id;
-    js.src = "//connect.facebook.net/ru_RU/sdk.js#xfbml=1&version=v2.5";
+    js.src = "//connect.facebook.net/en_US/sdk.js";
     fjs.parentNode.insertBefore(js, fjs);
 }(document, 'script', 'facebook-jssdk'));
