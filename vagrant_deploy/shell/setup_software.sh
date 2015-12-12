6694514495d67326e006b9da34deb4e7d0f31aee
v.krasikov <vl.itstorm@gmail.com>
2015-12-12 23:07:36 +0300

    Vagrantfile.dist

--- vagrant_deploy/shell/setup_software.sh
+++ vagrant_deploy/shell/setup_software.sh
@@ -8,15 +8,15 @@ apt-get -y install debconf-utils
 debconf-set-selections <<< 'mysql-server mysql-server/root_password password 12344321'
 debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password 12344321'
 
-apt-get -y install mysql-server=5.5.43-0+deb8u1
-apt-get -y install nginx=1.6.2-5
-apt-get -y install php5-cli=5.6.9+dfsg-0+deb8u1
-apt-get -y install php5-common=5.6.9+dfsg-0+deb8u1
-apt-get -y install php5-cgi=5.6.9+dfsg-0+deb8u1
-apt-get -y install php5-fpm=5.6.9+dfsg-0+deb8u1
-apt-get -y install php5-curl=5.6.9+dfsg-0+deb8u1
-apt-get -y install php5-intl=5.6.9+dfsg-0+deb8u1
-apt-get -y install php5-gd=5.6.9+dfsg-0+deb8u1
-apt-get -y install php5-mysql=5.6.9+dfsg-0+deb8u1
-apt-get -y install php5-xdebug=2.2.5-1
+apt-get -y install mysql-server
+apt-get -y install nginx
+apt-get -y install php5-cli
+apt-get -y install php5-common
+apt-get -y install php5-cgi
+apt-get -y install php5-fpm
+apt-get -y install php5-curl
+apt-get -y install php5-intl
+apt-get -y install php5-gd
+apt-get -y install php5-mysql
+apt-get -y install php5-xdebug
 apt-get -y autoremove
